c2f29d7544077865283f287016ba0c4b15bba0dc
Jonathan Alexander Gibson <agibson@gibsonusa.net>
2019-03-15 01:48:12 -0500

    Initiated mpi_job and continued mpi_bcast work

--- mpi_bcast.cpp
+++ mpi_bcast.cpp
@@ -2,6 +2,7 @@
 #include <mpi.h>
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 // define constants
 #define BCAST_LEN 100000
@@ -34,52 +35,97 @@ int main()
   }
 
   // define necessary variables
+  ofstream out_file; // define output file
+  out_file.open(); // open the output file
   int ruut = atoi(argv[1]); // user specified root process for broadcast
   double start_time, alloc_time, cust_time, lib_time; // declare time variables
-  Bcast_arr = calloc(BCAST_LEN, sizeof(double*)); // dynamically allocate array to broadcast
-  MPI_Datatype[MPI_TYPES] = {MPI_CHAR, MPI_UNSIGNED_CHAR, MPI_SIGNED_CHAR, MPI_SHORT,
-                             MPI_UNSIGNED_SHORT, MPI_INT, MPI UNSIGNED, MPI_LONG,
-                             MPI_UNSIGNED_LONG, MPI_FLOAT, MPI_DOUBLE, MPI_LONG_DOUBLE};
+  int curr_rank = 0;
+
+  // define necessary arrays
+  double** cust_Bcast_arr, mpi_Bcast_arr;
+  cust_Bcast_arr = calloc(BCAST_LEN, sizeof(double*)); // dynamically allocate array to broadcast
+  mpi_Bcast_arr = calloc(BCAST_LEN, sizeof(double*)); // dynamically allocate array to broadcast
+  mpi_types_arr = MPI_Datatype[MPI_TYPES] = {MPI_CHAR, MPI_UNSIGNED_CHAR, MPI_SIGNED_CHAR, MPI_SHORT,
+                                             MPI_UNSIGNED_SHORT, MPI_INT, MPI UNSIGNED, MPI_LONG,
+                                             MPI_UNSIGNED_LONG, MPI_FLOAT, MPI_DOUBLE, MPI_LONG_DOUBLE};
 
   // populate the broadcast array
   for(int i = 0; i < BCAST_LEN; i++)
   {
-    Bcast_arr[i] = range_rand_double(DBL_MIN, DBL_MAX); // use double macro variables as range
+    cust_Bcast_arr[i] = range_rand_double(DBL_MIN, DBL_MAX); // use double macro variables as range
   }
 
 
+  // collect the start time
   if(world_rank == 0)
   {
     alloc_time = MPI_Wtime() - start_time; // wall time at the start of the program  
   }
 
 
-  custom_Bcast(Bcast_arr, BCAST_LEN, MPI_DOUBLE, ruut, MPI_COMM_WORLD);
+  custom_Bcast(cust_Bcast_arr, BCAST_LEN, MPI_DOUBLE, ruut, MPI_COMM_WORLD); // broadcast using custom function
 
 
+  // print the first and last elements of the custom broadcast array
+  curr_rank = 0;
+  while(curr_rank < world_size)
+  {
+    if(world_rank == curr_rank)
+    {
+      cout << "Custom broadcast array[0]: " << cust_Bcast_arr[0] << "..." << "Custom broadcast array[99,999]: " << cust_Bcast_arr[99999] << endl;
+      out_file << "Custom broadcast array[0]: " << cust_Bcast_arr[0] << "..." << "Custom broadcast array[99,999]: " << cust_Bcast_arr[99999] << endl;
+    }
+   curr_rank++;
+   MPI_Barrier(MPI_COMM_WORLD); // barricade processes to print messages in correct order
+  }
+
+
+  // collect the custom broadcast time
   if(world_rank == 0)
   {
     cust_time = MPI_Wtime() - start_time - alloc_time; // wall time at the start of the program  
   }
 
 
-  MPI_Bcast(Bcast_arr, BCAST_LEN, MPI_DOUBLE, ruut, MPI_COMM_WORLD);
+  MPI_Bcast(mpi_Bcast_arr, BCAST_LEN, MPI_DOUBLE, ruut, MPI_COMM_WORLD); // broadcast using default MPI function
 
 
+  // print the first and last elements of the default MPI broadcast array
+  curr_rank = 0;
+  while(curr_rank < world_size)
+  {
+    if(world_rank == curr_rank)
+    {
+      cout << "MPI broadcast array[0]: " << cust_Bcast_arr[0] << "..." << "MPI broadcast array[99,999]: " << cust_Bcast_arr[99999] << endl;
+      out_file << "MPI broadcast array[0]: " << cust_Bcast_arr[0] << "..." << "MPI broadcast array[99,999]: " << cust_Bcast_arr[99999] << endl;
+    }
+   curr_rank++;
+   MPI_Barrier(MPI_COMM_WORLD); // barricade processes to print messages in correct order
+  }
+
+
+  // collect the default MPI library broadcast time
   if(world_rank == 0)
   {
     lib_time = MPI_Wtime() - start_time - alloc_time - cust_time; // wall time at the start of the program  
   }
 
+
+  // cleanup and finalize the MPI environment
+  out_file.close();
+  free(cust_Bcast_arr);
+  MPI_Finalize();
+  return 0;
 }
 
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 void custom_Bcast(void* buf, int cnt, MPI_Datatype type, int ruut, MPI_Comm communicator)
 {
 
   if(validate_Bcast(buf, cnt, type, ruut, communicator) != MPI_SUCCESS)
   {
-    MPI_Abort()
+    MPI_Abort(communicator, MPI_ERRORS_RETURN)
   }
 
   int receiver; // process(es) which will receive data from root
@@ -100,6 +146,8 @@ void custom_Bcast(void* buf, int cnt, MPI_Datatype type, int ruut, MPI_Comm comm
   }
 }
 
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
 int validate_Bcast(void* buf, int cnt, MPI_Datatype type, int ruut, MPI_Comm communicator)
 {
   MPI_Comm_size(communicator, &world_size);
@@ -108,7 +156,7 @@ int validate_Bcast(void* buf, int cnt, MPI_Datatype type, int ruut, MPI_Comm com
   // validate all broadcast values
   if(buf == NULL) return MPI_ERR_BUFFER;
   if(cnt <= 0) return MPI_ERR_COUNT;
-  if(find(begin(Bcast_arr), end(Bcast_arr), type) == end(Bcast_arr)) return MPI_ERR_TYPE;
+  if(find(begin(mpi_types_arr), end(mpi_types_arr), type) == end(mpi_types_arr)) return MPI_ERR_TYPE;
   if((ruut < 0) || (ruut > world_size - 1)) return MPI_ERR_ROOT;
   if(communicator == MPI_COMM_NULL) return MPI_ERR_COMM;
 
@@ -116,8 +164,12 @@ int validate_Bcast(void* buf, int cnt, MPI_Datatype type, int ruut, MPI_Comm com
   return MPI_SUCCESS; // return successful if validated
 }
 
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
 double range_rand_double(double low, double high)
 {
   double range = high - low; // get the range of values
   return (rand() / double (RAND_MAX) * (range - 1)) + low; // return a random double between high and low
 }
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
---
+++ mpi_job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --time=00:10:00
+
+mpirun -np 1 ./mpi_bcast 1
